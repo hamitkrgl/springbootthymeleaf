d347f76a033f9f9a92218bcf0a56a3f17112b696
Hamit Köroğlu <hamitkrgl@icloud.com>
2022-07-23 00:16:13 +0300

    Delete spring-boot-teknikservis-main directory

--- spring-boot-teknikservis-main/src/main/java/com/bilgeadam/springbootteknikservis/repo/ServiceRepo.java
+++
@@ -1,8 +0,0 @@
-package com.bilgeadam.springbootteknikservis.repo;
-
-import com.bilgeadam.springbootteknikservis.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepo extends JpaRepository<Service, Long>
-{
-}
